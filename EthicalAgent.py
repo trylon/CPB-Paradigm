a6f6b0287c8b6c3f4abf97fb912d4fc706c67876
Robert Stolarz <robert.stolarz@yandex.com>
2014-10-19 14:34:38 -0400

    Add getPreferredAction (which randomly picks if there are multiple applicable actions)

--- EthicalAgent.py
+++ EthicalAgent.py
@@ -44,3 +44,16 @@ class EthicalAgent:
             if all(x >= y for x, y in zip(principleClause, result)):
                 return i
         return -1 #none found
+    
+    def getPreferredAction(self,actionList):
+      sorted = self.mySorted(actionList)
+      bestActions = [] #GC warning spot
+      i = 0
+      while True: #emulate do-while http://stackoverflow.com/a/743944/1376005
+        bestActions.append(sorted[i]) #add the action (do this before checking because you need at least one)
+        if i < len(sorted) - 1 #if the length of the array has not been iterated
+          and self.isActionPreferable(sorted[i],sorted[i+1]) == 0: #and the actions are equally preferable
+            i = i + 1 #i++ and continue
+        else:  #otherwise
+          break #stop the loop
+      return random.choice(bestActions) #pick randomly from the generated list http://stackoverflow.com/questions/306400/how-do-i-randomly-select-an-item-from-a-list-using-python
